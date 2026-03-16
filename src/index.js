85e9339609c6dfa3b7cc6e163a5108eb28480a49
Mike Sapp <mikesapp@gmail.com>
2026-03-15 22:04:16 -0400

    Initial Cloudflare Worker

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export default {
+  async fetch(request) {
+    return new Response("Hello from Cloudflare Workers!", {
+      headers: {
+        "content-type": "text/plain; charset=UTF-8"
+      }
+    });
+  }
+};
